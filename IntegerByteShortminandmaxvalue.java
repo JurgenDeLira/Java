178759cc72ba50dc8f6d4a29031a770a49c0ec8d
Jürgen De Lira <106788934+JurgenDeLira@users.noreply.github.com>
2024-02-20 09:35:46 -0700

    Create IntegerByteShortminandmaxvalue.java
    This will return those values if you type them in JShell

---
+++ IntegerByteShortminandmaxvalue.java
@@ -0,0 +1,3 @@
+System.out.print("Integer Value Range (" + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE + ")");
+System.out.print("Byte Value Range (" + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE + ")");
+System.out.print("Short Value Range (" + Short.MIN_VALUE + " to " + Short.MAX_VALUE + ")");
