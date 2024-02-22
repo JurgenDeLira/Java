0addb8db347d5c6ee2daf12051fa15b5e38d222d
Jürgen De Lira <106788934+JurgenDeLira@users.noreply.github.com>
2024-02-22 11:28:35 -0700

    Ejemplos de Char y Boolean

---
+++ CharYBoolean.java
@@ -0,0 +1,11 @@
+char myChar = 'D';
+/*char myChar = 'DD'; este dara error */
+char myChar = 'D';
+char myUnicode = '\u0044';
+char myDecimalCode = 68;
+char mySimpleChar = '?';
+char mySimpleChar = '?'; char  myUnicodeChar = '\u003F'; char myDecimalChar = 63;
+System.out.print("My values are " + mySimpleChar + myUnicodeChar + myDecimalChar);
+boolean myTrueBooleanValue = true;
+boolean myFalseBooleanValue = false;
+boolean isCustomerOverTwentyOne = true;
