5f3e947ae08edafd67db1cfd3c9689177cdd0fd2
Jürgen De Lira <106788934+JurgenDeLira@users.noreply.github.com>
2024-02-22 13:06:52 -0700

    Create RepasoStringsNumeros.java

---
+++ RepasoStringsNumeros.java
@@ -0,0 +1,11 @@
+String myString = "This is a string";
+System.out.print("myString is equal to " + myString);
+myString = myString + ", and this is more.";
+System.out.print("myString is equal to " +  myString);
+myString = "I wish I had \u00241,000,000.00"
+   {
+   ...>     String numberString = "250.55";
+   ...>     numberString = numberString + "49.45";
+   ...>     System.out.print(numberString);
+   ...> }
+String numberString = "250.55"; numberString = numberString + "49.45"; System.out.print(numberString);
